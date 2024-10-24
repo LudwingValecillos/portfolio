2a1cc9ac12a383ed7ace8ae09096ab8d527e85cc
LudwingValecillos <ludwingvaldev@gmail.com>
2024-10-24 11:50:45 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -68,7 +68,7 @@ function App() {
   const contactLinks = [
     { icon: FaGithub, link: "https://github.com/LudwingValecillos", label: "GitHub" },
     { icon: FaLinkedin, link: "https://www.linkedin.com/in/ludwingvalecillos/", label: "LinkedIn" },
-    { icon: FaFileAlt, link: "/public/Ludwing Valecillos cv.pdf", label: language === 'en' ? "CV" : "Currículum" },
+    { icon: FaFileAlt, link: "../src/assets/LudwingValecillosCV.pdf", label: language === 'en' ? "CV" : "Currículum" },
     { icon: FaEnvelope, link: "mailto:ludwingvaldev@gmail.com", label: language === 'en' ? "Email" : "Correo" }
   ]
 
